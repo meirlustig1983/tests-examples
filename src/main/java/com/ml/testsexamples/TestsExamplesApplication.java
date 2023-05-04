301f57ad3454b0e4a46b2478a43398165be48ea4
meirlustig <meirlustig1983@gmail.com>
2023-05-04 16:28:08 -0700

    TE-001 First commit

---
+++ src/main/java/com/ml/testsexamples/TestsExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.ml.testsexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestsExamplesApplication.class, args);
+    }
+
+}
