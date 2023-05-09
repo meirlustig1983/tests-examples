2d4ced9f2ba02b4861ea5b0c91d6649087a52865
meirlustig <meirlustig1983@gmail.com>
2023-05-08 19:27:47 -0700

    TE-008 Add DI test for BankService

---
+++ src/test/java/com/ml/testsexamples/dao/BankAccountParameterResolver.java
@@ -0,0 +1,28 @@
+package com.ml.testsexamples.dao;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+
+import java.math.BigDecimal;
+
+public class BankAccountParameterResolver implements ParameterResolver {
+    @Override
+    public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        return parameterContext.getParameter().getType() == BankAccount.class;
+    }
+
+    @Override
+    public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        BankAccount.BankAccountBuilder builder = BankAccount.builder();
+        return builder
+                .id(1L)
+                .firstName("Theodore")
+                .lastName("Roosevelt")
+                .balance(BigDecimal.valueOf(3500))
+                .minimumBalance(BigDecimal.valueOf(1500))
+                .active(true)
+                .build();
+    }
+}
