098144bc7b8e90c7bbcb758cc0db88d3ddcf3155
meirlustig <meirlustig1983@gmail.com>
2023-05-16 21:14:36 -0700

    TE-015 Change Hibernate settings for tests

--- src/test/java/com/ml/testsexamples/services/BankAccountServiceIT.java
+++ src/test/java/com/ml/testsexamples/services/BankAccountServiceIT.java
@@ -15,7 +15,6 @@ import org.springframework.test.context.jdbc.Sql;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
