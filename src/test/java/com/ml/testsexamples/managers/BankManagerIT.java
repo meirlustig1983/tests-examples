58de94cb916a99eff27f6a40ef40b631488f1305
meirlustig <meirlustig1983@gmail.com>
2023-05-16 20:55:34 -0700

    TE-014 Examples for @EnabledOnOs and @EnabledOnJre - test #7

--- src/test/java/com/ml/testsexamples/managers/BankManagerIT.java
+++ src/test/java/com/ml/testsexamples/managers/BankManagerIT.java
@@ -57,7 +57,7 @@ public class BankManagerIT {
     @Test
     @DisplayName("Test get info about bank account, if (OS is LINUX). BankAccount[id = 1]")
     @Timeout(value = 2000, unit = TimeUnit.MICROSECONDS)
-    @EnabledOnOs({OS.MAC})
+    @EnabledOnOs({OS.LINUX})
     public void info_EnabledOnOsLINUX() {
 
         Optional<BankAccount> result = manager.info(1L);
